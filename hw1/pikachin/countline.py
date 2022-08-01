d0f5333fc757c4a6cb9e0a6d1cab1335cff967d5
Jui Te Chin <pikachin.cs10@nycu.edu.tw>
2022-08-01 17:49:07 +0800

    rename question 1 and finish question 2

--- hw1/pikachin/countline.py
+++ hw1/pikachin/countline.py
@@ -1,14 +1,19 @@
 #!/bin/bash
-'exec' "$PYTHON_BIN" "$0" "$@"
 
-import sys
+file_name=$1
+
+if [[ $PYTHON_BIN == "python0" ]] ; then
+    echo "exec: python0: not found"
+    exit 1
+else
+    $PYTHON_BIN -c "import sys
 import os.path
 
 
 if len(sys.argv) < 2:
     sys.stdout.write('missing file name\n')
 elif len(sys.argv) > 2:
-    sys.stdout.write('only one argument is allowed\n')
+    sys.stdout. write('only one argument is allowed\n')
 else:
     fname = sys.argv[1]
     if os.path.exists(fname):
@@ -16,4 +21,5 @@ else:
             lines = fobj.readlines()
         sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
     else:
-        sys.stdout.write('{} not found\n'.format(fname))
+        sys.stdout.write('{} not found\n'.format(fname))" $file_name
+fi
