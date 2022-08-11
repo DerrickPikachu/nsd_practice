ccfb0e1a79d31095cb59f7e3f08a96b89aa85bed
DerrickPikachu <derrfgh4563@gmail.com>
2022-08-11 16:15:00 +0800

    complete q2

--- hw2/DerrickPikachu/q1/line.cpp
+++ hw2/DerrickPikachu/q1/line.cpp
@@ -2,6 +2,6 @@
 
 Line::Line(Line const & other) { coord_ = other.coord_; }
 Line::Line(Line && other) { coord_ = std::move(other.coord_); }
-Line & Line::operator=(Line const & other) { coord_ = other.coord_; }
-Line & Line::operator=(Line && other) { coord_ = std::move(other.coord_); }
+Line & Line::operator=(Line const & other) { coord_ = other.coord_; return *this; }
+Line & Line::operator=(Line && other) { coord_ = std::move(other.coord_); return *this; }
 Line::Line(size_t size) { coord_.resize(size, std::vector<float>(2)); }
